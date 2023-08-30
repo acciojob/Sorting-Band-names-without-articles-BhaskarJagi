9dba85ea4feb5839b1bf44a397302405a0227af7
BhaskarJagi <128607429+BhaskarJagi@users.noreply.github.com>
2023-08-30 23:03:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 //your code here
 
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+
+touristSpots = touristSpots.map((word) => {
+	let articles = ['a', 'an', 'The', 'A', 'An', 'the']
+	if(articles.includes(word.split(" ")[0])){
+		word = word.split(' ')
+		word.shift()
+    	return word.join(" ");
+	}else{
+    	return word;
+  	}
+})
+
+let ul = document.getElementsByTagName('ul')[0];
+
+touristSpots.sort().forEach((word) => {
+	let li = document.createElement('li');
+	li.textContent = word;
+	ul.append(li)
+})
+
